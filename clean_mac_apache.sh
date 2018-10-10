3dd7155022e582dffa853b84f5cc4392a6e9feed
梁金辉 <liangjinhui@pagoda.com.cn>
2018-10-10 18:48:26 +0800

    SR: memcached phalcontools beanstalkd

---
+++ clean_mac_apache.sh
@@ -0,0 +1,14 @@
+#要停止apache服务：sudo apachectl stop
+#删除如下apache目录：
+sudo rm -rf /etc/apache2
+sudo rm -rf /usr/include/apahce2
+sudo rm -rf /usr/libexec/apache2
+#删除php目录，（有点多）：
+sudo rm -rf /usr/php
+sudo rm -rf /usr/bin/php
+sudo rm -rf /usr/bin/php-config
+sudo rm -rf /usr/bin/phpize
+sudo rm -rf /usr/include/php
+sudo rm -rf /usr/lib/php
+sudo rm -rf /usr/share/man/man*/php*
+
